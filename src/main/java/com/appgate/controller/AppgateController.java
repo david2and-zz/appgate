48072c1e69a70513313577dd2242e5295ee29fdd
David Andres Perez Gomez <david.perez@avaldigitallabs.com>
2021-02-22 10:49:18 -0500

    :construction: Create proyect

---
+++ src/main/java/com/appgate/controller/AppgateController.java
@@ -0,0 +1,8 @@
+package com.appgate.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppgateController {
+
+}
---
+++ src/main/java/com/appgate/service/AppgateService.java
@@ -0,0 +1,7 @@
+package com.appgate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppgateService {
+}
